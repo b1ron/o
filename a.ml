3e074521161cbbea534c4a963e274d333e183795
b1ron <goldingbyron@gmail.com>
2023-08-20 23:45:05 +0200

    some more

--- a.ml
+++ a.ml
@@ -9,3 +9,22 @@ let rec read_and_accumulate accum =
 
 let () =
   printf "Total: %F\n" (read_and_accumulate 0.)
+
+let rec sum l =
+  match l with
+  | [] -> 0 (* base case *)
+  | hd :: tl -> hd + sum tl (* inductive case *)
+
+  (* Logically, you can think of the evaluation of a simple recursive function like 
+  sum almost as if it were a mathematical equation whose meaning you were unfolding step by step
+  sum [1;2;3]
+  = 1 + sum [2;3]
+  = 1 + (2 + sum [3])
+  = 1 + (2 + (3 + sum []))
+  = 1 + (2 + (3 + 0))
+  = 1 + (2 + 3)
+  = 1 + 5
+  = 6 *)
+
+(* a recursive function to compute the n-th fibonacci number *)
+let rec fib n =
