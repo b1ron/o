e4f619558236d0be838087d95830d7da7b5cea30
b1ron <goldingbyron@gmail.com>
2023-08-22 04:19:51 +0200

    more

--- a.ml
+++ a.ml
@@ -1,6 +1,8 @@
 open Base
 open Stdio
 
+(* TODO setup Dune for automatic formatting *)
+
 let rec read_and_accumulate accum =
   let line = In_channel.input_line In_channel.stdin in
   match line with
@@ -27,4 +29,27 @@ let rec sum l =
   = 6 *)
 
 (* a recursive function to compute the n-th fibonacci number *)
+(* TODO tail recursion is more interesting *)
 let rec fib n =
+  if n = 0 then 0
+  else if n = 1 then 1
+  else fib (n - 1) + fib (n - 2);;
+
+let rec remove_sequential_duplicates list =
+  match list with
+  | [] -> []
+  | [x] -> [x]
+  | first :: second :: tl ->
+    if first = second then
+      remove_sequential_duplicates (second :: tl)
+    else
+    first :: remove_sequential_duplicates (second :: tl);;
+
+type point2d = { x : float; y : float };;
+
+(* field punning *)
+let magnitude { x; y } = Float.sqrt (x **. 2. +. y **. 2.);;
+
+(* dot notation *)
+let distance v1 v2 =
+  magnitude { x = v1.x -. v2.x; y = v1.y -. v2.y };;
